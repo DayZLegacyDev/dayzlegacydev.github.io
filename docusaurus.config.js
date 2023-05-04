576001ecd6d338c871e9e426f4cdd679a999de61
doctashay <doctashay@github.com>
2023-05-03 18:41:16 -0600

    Test DocuSaurus push

---
+++ docusaurus.config.js
@@ -0,0 +1,181 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/okaidia');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'OpenGOAL',
+  tagline: 'Reviving the Language that Brought us the Jak and Daxter Series',
+  url: 'https://open-goal.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.png',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'open-goal', // Usually your GitHub org/user name.
+  projectName: 'open-goal.github.io', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  markdown: {
+    mermaid: true,
+  },
+  scripts: [
+    {
+      src: 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.74/dist/shoelace.js',
+      type: "module",
+    }
+  ],
+  stylesheets: [
+    'https://fonts.googleapis.com/css2?family=Lato:wght@400;500;700&family=Roboto+Mono:wght@400;700&display=swap',
+    'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.74/dist/themes/dark.css',
+    "https://fonts.googleapis.com/icon?family=Material+Icons"
+  ],
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: "docs",
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/open-goal/open-goal.github.io/tree/master/'
+        },
+        blog: {
+          path: 'blog',
+          blogSidebarCount: 0,
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/open-goal/open-goal.github.io/tree/master/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      announcementBar: {
+        id: 'announcementBar-2', // Increment on change
+        content: `<a href="/blog/progress-report-apr-2023">Check out April's Progress Report Here!</a>`,
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'YAP33BKRCA',
+        // Public API key: it is safe to commit it
+        apiKey: '7d68c3181a134366b669225073fed1cb',
+        indexName: 'open-goal',
+        // Optional: see doc section below
+        contextualSearch: true,
+        // Optional: Algolia search parameters
+        searchParameters: {},
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+        //... other Algolia params
+      },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
+      image: 'img/logo.png',
+      metadata: [{ name: 'twitter:card', content: 'summary' }],
+      navbar: {
+        title: 'OpenGOAL',
+        logo: {
+          alt: 'OpenGOAL Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          { to: 'blog', label: 'Blog', position: 'left' },
+          {
+            to: '/progress/milestones', label: 'Progress', position: 'left',
+            items: [
+              {
+                label: 'Major Milestones',
+                to: '/progress/milestones',
+              },
+              {
+                label: 'Jak and Daxter - Decompilation',
+                to: '/progress/jak1',
+              },
+              {
+                label: 'Jak II - Decompilation',
+                to: '/progress/jak2',
+              }
+            ]
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentation',
+          },
+          { to: '/docs/faq', label: 'FAQ', position: 'left' },
+          {
+            label: 'Galleries', position: 'left',
+            items: [
+              {
+                label: 'Jak and Daxter: The Precursor Legacy',
+                to: '/gallery/jak1',
+              },
+              {
+                label: 'Jak II',
+                to: '/gallery/jak2',
+              }
+            ]
+          },
+          {
+            href: 'https://github.com/open-goal',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `
+        Copyright © ${new Date().getFullYear()} OpenGOAL. Built with Docusaurus.
+        <br>
+        <br>
+        <span class="trademark-disclaimer">All third party trademarks (including but not limited to: logos and icons) referenced by OpenGOAL remain the property of their respective owners. Unless specifically identified as such, OpenGOAL's use of third party trademarks does not indicate any relationship, sponsorship, or endorsment between OpenGOAL and the owners of these trademarks.  Any references to the aforementioned trademarks are for informative purposes and should be considered nominative fair use.</span>`,
+      },
+      prism: {
+        theme: darkCodeTheme,
+        lightTheme: lightCodeTheme,
+        additionalLanguages: ['lisp', 'clojure']
+      },
+    }),
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('swc-loader'),
+      options: {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          target: 'es2017',
+        },
+        module: {
+          type: isServer ? 'commonjs' : 'es6',
+        },
+      },
+    }),
+  },
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import { useState, useEffect, useRef } from 'react';
+import Layout from '@theme/Layout';
+import LatestBlogs from '@site/src/components/LatestBlogs';
+import LauncherDownloadLink from '@site/src/components/LauncherDownloadLink';
+
+const FeatureList = [
+  {
+    title: 'Performance Overhauls',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+          Improving both client and server performance through the use of improved rendering and processing techniques is a primary goal for us.
+      </>
+    ),
+  },
+  {
+    title: 'Quality of Life Fixes',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        DayZ Legacy is built with the intention of delivering critical quality of life fixes to resolve some of the most pressing issues in these older game versions.
+      </>
+    ),
+  },
+  {
+    title: 'Custom Anti-Cheat' ,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Our Helix anti-cheat proactively monitors our servers for modified or malicious activity.
+      </>
+    ),
+  },
+  {
+    title: 'Relive the era of old-school DayZ' ,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Never had a chance to play old-school DayZ or want to relive the moment? That's exactly what our project enables players to do.
+      </>
+    ),
+  },
+  {
+    title: 'New Additions and Features' ,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        A major goal of this project is to have a "best-of-both-worlds" between new and old DayZ. Better land, sea, and aerial vehicles. Expanded horticulture. Traders and safezones. New and custom weapons. Persistent loot economy.
+      </>
+    ),
+  },
+  {
+    title: 'Audio and Visual Overhauls' ,
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Some of our currently implemented visual changes include a DirectX 11 and Vulkan build (DX12 TBA!), water caustics and refractions, FXAA edge smoothing, and improved Alpha to Coverage/Render to Texture properties. We have plans to integrate new visual effects like HDAO+, and re-integrate old ones like SSAO and volumetric clouds.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
